215889f7478a2eee8f79e33ca6d99408ef4bde1f
DavidGarciaReynoso <davidgarciareynoso903@gmail.com>
2023-05-20 14:19:35 -0600

    se añadio funcionMultiplo

---
+++ 5 V4TRIFIV/src/funciones/funcionMultiplo.java
@@ -0,0 +1,22 @@
+package funciones;
+/**
+ *
+ * @author Todos
+ */
+public class funcionMultiplo {
+    public String retornaMultiplo(int num){
+    
+//Proceso de Datos
+        
+if(num%3==0 && num%5==0){
+        return "TRIFV";
+    }
+        else if(num%5==0){
+            return "FIV";
+        }
+        else if(num%3==0){
+            return "TRI";
+        }
+        else return "ningun valor";
+    }
+}
