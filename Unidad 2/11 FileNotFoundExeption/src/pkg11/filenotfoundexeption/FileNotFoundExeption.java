43986e0e66940f0db4af707a418dd1c66473d8c1
DavidGarciaReynoso <davidgarciareynoso903@gmail.com>
2023-07-02 18:43:26 -0600

    se añadió el programa 11

---
+++ Unidad 2/11 FileNotFoundExeption/src/pkg11/filenotfoundexeption/FileNotFoundExeption.java
@@ -0,0 +1,25 @@
+package pkg11.filenotfoundexeption;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileNotFoundExeption {
+
+    public static void main(String[] args) {
+        
+    try {
+            // Intentar leer un archivo inexistente
+            File file = new File("archivo.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Se produjo una excepción FileNotFoundException: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
